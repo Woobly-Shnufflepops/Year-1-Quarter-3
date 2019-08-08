d41beedd1d01d342cb3c82d3211ce7775a611c90
Woobly <halfcounter55@hotmail.com>
2019-08-07 22:51:51 -0700

    Code and documents from Renton Technical College summer quarter Year 1 Quarter 3 2017

---
+++ LoginTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using John.SocialClub.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace John.SocialClub.Desktop.Tests
+{
+    [TestClass()]
+    public class LoginTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using John.SocialClub.Desktop;
+using John.SocialClub.Desktop.Properties;
+
+namespace John.SocialClub.DesktopTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool isvalid = Login.validateLogin(Settings.Default.Username, Settings.Default.Password);
+            
+            Assert.IsTrue(isvalid);
+        }
+    }
+}
